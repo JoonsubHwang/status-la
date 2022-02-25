e076597f0a8ea2217fa000a1051fcb925eb237f7
JoonsubHwang <j97194719@gmail.com>
2022-02-24 22:00:56 -0500

    Added commands

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
 
 
 const client = new Client({
@@ -13,8 +15,26 @@ client.once('ready', () => {
     console.log('ready');
 });
 
-// client.on('messageCreate', msg => {
+client.on('interactionCreate', async interaction => {
 
-// });
+    if (!interaction.isCommand)
+        return;
 
-client.login(process.env.TOKEN);
+    const { commandName } = interaction;
+    
+    if (commandName === 'setserver') {
+        await interaction.reply('Server set to **Elzowin**.');
+    }
+    else if (commandName === 'server') {
+        await interaction.reply('Elzowin: g');
+    }
+    else if (commandName === 'all') {
+        await interaction.reply('Elzowin: g \nAvesta: g');
+    }
+    else if (commandName === 'help') {
+        await interaction.reply('\`/setserver\` Set default server to display. \n\`/server\` Display status of a server. \n\`/all\` Display status of all servers.');
+    }
+
+});
+
+client.login(token);
